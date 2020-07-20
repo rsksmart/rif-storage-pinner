3605df6aede6f69de56f550514f45f6259a93a07
Adam Uhlíř <adam@uhlir.dev>
2020-07-20 10:47:39 +0200

    feat: pinning gc (#81)

---
+++ src/gc.ts
@@ -0,0 +1,51 @@
+import { BlockHeader } from 'web3-eth'
+import config from 'config'
+
+import Agreement from './models/agreement.model'
+import { Op } from 'sequelize'
+import { ProviderManager } from './providers'
+import { loggingFactory } from './logger'
+
+const logger = loggingFactory('gc')
+
+/**
+ * This is a closure that garbage-collects pins of expired Agreements.
+ *
+ * When first detects an Agreement to have no funds it saves the block number at what it was detected.
+ * Then after number of blocks specified for confirmations it actualy unpins the data.
+ *
+ * This is in order to handle case when reorg happens and the DepositFunds event is emitted in the
+ * confirmation range.
+ *
+ * @param manager
+ */
+export function collectPinsClosure (manager: ProviderManager) {
+  return async (block: BlockHeader): Promise<void> => {
+    logger.verbose('Running pinning GC')
+
+    const nonExpiredAgreements = await Agreement.findAll({ where: { expiredAtBlockNumber: null, isActive: true } })
+    for (const agreement of nonExpiredAgreements) {
+      if (!agreement.hasSufficientFunds) {
+        logger.verbose(`Marking agreement ${agreement.agreementReference} to be collected.`)
+        agreement.expiredAtBlockNumber = block.number
+        await agreement.save()
+      }
+    }
+
+    const agreementsToUnpins = await Agreement.findAll({
+      where: {
+        expiredAtBlockNumber: {
+          [Op.lte]: block.number - config.get<number>('blockchain.eventsEmitter.confirmations')
+        },
+        isActive: true
+      }
+    })
+    for (const agreement of agreementsToUnpins) {
+      logger.info(`Unpinning agreement ${agreement.agreementReference}.`)
+      await manager.unpin(agreement.dataReference)
+
+      agreement.isActive = false
+      await agreement.save()
+    }
+  }
+}
--- test/integration/index.spec.ts
+++ test/integration/index.spec.ts
@@ -2,14 +2,19 @@ import chai from 'chai'
 import dirtyChai from 'dirty-chai'
 import { CID, IpfsClient } from 'ipfs-http-client'
 
-import { AppSingleton, asyncIterableToArray, sleep, encodeHash, App, errorSpy } from '../utils'
+import { TestingApp, asyncIterableToArray, sleep, encodeHash, errorSpy } from '../utils'
 import { loggingFactory } from '../../src/logger'
 
 chai.use(dirtyChai)
-
 const logger = loggingFactory('test:pinning')
-
 const expect = chai.expect
+
+interface File {
+  fileHash: string
+  size: number
+  cid: CID
+  cidString: string
+}
 
 const isPinned = async (ipfs: IpfsClient, cid: CID): Promise<boolean> => {
   try {
@@ -21,7 +26,26 @@ const isPinned = async (ipfs: IpfsClient, cid: CID): Promise<boolean> => {
   }
 }
 
-const uploadRandomData = async (ipfs: IpfsClient): Promise<{ fileHash: string, size: number, cid: CID, cidString: string }> => {
+async function createAgreement (app: TestingApp, file: File, billingPeriod: number, money: number, size?: number): Promise<string> {
+  const encodedFileHash = encodeHash(file.fileHash)
+
+  const agreementGas = await app.contract
+    ?.methods
+    .newAgreement(encodedFileHash, app.providerAddress, size ?? file.size, billingPeriod, [])
+    .estimateGas({ from: app.consumerAddress, value: money })
+
+  const receipt = await app.contract
+    ?.methods
+    .newAgreement(encodedFileHash, app.providerAddress, size ?? file.size, billingPeriod, [])
+    .send({ from: app.consumerAddress, gas: agreementGas, value: money })
+  logger.info('Agreement created')
+
+  await app.advanceBlock()
+
+  return receipt.events.NewAgreement.returnValues.agreementReference
+}
+
+const uploadRandomData = async (ipfs: IpfsClient): Promise<File> => {
   const [file] = await asyncIterableToArray(ipfs.add([
     {
       path: `${Math.random().toString(36).substring(7)}.txt`,
@@ -35,94 +59,56 @@ const uploadRandomData = async (ipfs: IpfsClient): Promise<{ fileHash: string, s
   }
 }
 
-const getAgreementReference = (receipt: { events: { [k: string]: { returnValues: { agreementReference: string } } } }): string => {
-  if (!receipt.events['NewAgreement']) throw new Error('Agreement is not created')
-  return receipt.events['NewAgreement'].returnValues.agreementReference
-}
-
 describe('Pinning service', function () {
   this.timeout(100000)
-  let app: App
+  let app: TestingApp
 
   before(async () => {
-    app = await AppSingleton.getApp()
+    app = await TestingApp.getApp()
   })
 
   it('should pin hash on NewAgreement', async () => {
-    const file = await uploadRandomData(app.ipfsConsumer)
+    const file = await uploadRandomData(app.ipfsConsumer!!)
     // Check if not pinned
-    expect(await isPinned(app.ipfsProvider, file.cid)).to.be.false()
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.false()
 
-    const encodedFileHash = encodeHash(file.fileHash)
-
-    const agreementGas = await app.contract
-      ?.methods
-      .newAgreement(encodedFileHash, app.providerAddress, file.size, 1, [])
-      .estimateGas({ from: app.consumerAddress, value: 10000 })
-
-    await app.contract
-      ?.methods
-      .newAgreement(encodedFileHash, app.providerAddress, file.size, 1, [])
-      .send({ from: app.consumerAddress, gas: agreementGas, value: 10000 })
-    logger.info('Agreement created')
+    await createAgreement(app, file, 1, 10000)
 
     // Wait until we receive Event
     await sleep(1000)
 
-    expect(await isPinned(app.ipfsProvider, file.cid)).to.be.true()
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.true()
   })
+
   it('should reject if size limit exceed', async () => {
-    const file = await uploadRandomData(app.ipfsConsumer)
+    const file = await uploadRandomData(app.ipfsConsumer!)
     // Check if not pinned
-    expect(await isPinned(app.ipfsProvider, file.cid)).to.be.false()
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.false()
 
-    const encodedFileHash = encodeHash(file.fileHash)
-
-    const agreementGas = await app.contract
-      ?.methods
-      .newAgreement(encodedFileHash, app.providerAddress, file.size - 1, 1, [])
-      .estimateGas({ from: app.consumerAddress, value: 10000 })
-
-    await app.contract
-      ?.methods
-      .newAgreement(encodedFileHash, app.providerAddress, file.size - 1, 1, [])
-      .send({ from: app.consumerAddress, gas: agreementGas, value: 10000 })
-    logger.info('Agreement created')
+    await createAgreement(app, file, 1, 10000, file.size - 1)
 
     // Wait until we receive Event
     await sleep(1000)
 
     // Should not be pinned
-    expect(await isPinned(app.ipfsProvider, file.cid)).to.be.false()
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.false()
     expect(errorSpy.calledOnce).to.be.eql(true)
     const [error] = errorSpy.lastCall.args
     expect(error).to.be.instanceOf(Error)
     expect(error.message).to.be.eql('The hash exceeds payed size!')
   })
-  it('should unpin when agreement is expired', async () => {
-    const file = await uploadRandomData(app.ipfsConsumer)
+
+  it('should unpin when agreement is stopped', async () => {
+    const file = await uploadRandomData(app.ipfsConsumer!)
     // Check if not pinned
-    expect(await isPinned(app.ipfsProvider, file.cid)).to.be.false()
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.false()
 
-    const encodedFileHash = encodeHash(file.fileHash)
-
-    const agreementGas = await app.contract
-      ?.methods
-      .newAgreement(encodedFileHash, app.providerAddress, file.size, 1, [])
-      .estimateGas({ from: app.consumerAddress, value: 500 })
-
-    const receipt = await app.contract
-      ?.methods
-      .newAgreement(encodedFileHash, app.providerAddress, file.size, 1, [])
-      .send({ from: app.consumerAddress, gas: agreementGas, value: 500 })
-    logger.info('Agreement created')
-
-    const agreementReference = getAgreementReference(receipt)
+    const agreementReference = await createAgreement(app, file, 1, 500)
 
     await sleep(1000)
 
     // Should be pinned
-    expect(await isPinned(app.ipfsProvider, file.cid)).to.be.true()
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.true()
 
     const payoutGas = await app.contract
       ?.methods
@@ -135,10 +121,37 @@ describe('Pinning service', function () {
       .send({ from: app.providerAddress, gas: payoutGas })
     logger.debug('Payed out')
 
+    await app.advanceBlock()
+
     // Wait until we receive Event
     await sleep(1000)
 
     // Should not be be pinned
-    expect(await isPinned(app.ipfsProvider, file.cid)).to.be.false()
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.false()
+  })
+
+  it('should unpin when agreement run out of funds', async () => {
+    const file = await uploadRandomData(app.ipfsConsumer!)
+    // Check if not pinned
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.false()
+
+    await createAgreement(app, file, 1, 500)
+    await sleep(500)
+
+    // Should be pinned
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.true()
+
+    // First lets the time fast forward so the Agreement runs out of funds
+    await sleep(3000)
+
+    // Create new block to
+    await app.advanceBlock()
+    await sleep(200)
+    await app.advanceBlock()
+
+    await sleep(1500)
+
+    // Should not be be pinned
+    expect(await isPinned(app.ipfsProvider!, file.cid)).to.be.false()
   })
 })
