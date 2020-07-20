95f5991b0554f18efd5f13ddf66ba8c3266d113f
Adam Uhlíř <adam@uhlir.dev>
2020-07-20 11:15:04 +0200

    chore: dependencies cleanup

--- .tasegir.js
+++ .tasegir.js
@@ -1,5 +1,5 @@
 module.exports = {
   depCheck: {
-    ignore: ['colors', 'sinon', 'typedoc', 'typedoc-plugin-markdown', '@types/*', 'tasegir',  '@oclif/config', '@oclif/plugin-help', 'bn.js', 'cli-ux', 'oclif', 'reflect-metadata', 'sqlite3', '@fluffy-spoon/substitute', '@oclif/dev-cli', 'chai-as-promised', 'cross-env', 'dirty-chai', 'globby', 'ipfs', 'ipfsd-ctl', 'merge-options', 'sinon-chai', 'typescript']
+    ignore: ['typedoc', 'typedoc-plugin-markdown', '@types/*', 'tasegir',  '@oclif/*', 'reflect-metadata', 'sqlite3', 'cross-env']
   }
 }
