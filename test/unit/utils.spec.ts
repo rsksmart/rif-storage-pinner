e8fef552d6c90a3fda39fb2810a888ac9806bc7f
Nazar Duchak <41945483+nduchak@users.noreply.github.com>
2020-11-19 16:07:31 +0200

    fix: fix bytes to megabytes convertion (#257)

--- test/unit/utils.spec.ts
+++ test/unit/utils.spec.ts
@@ -1,7 +1,9 @@
 import chai from 'chai'
 import dirtyChai from 'dirty-chai'
+import BigNumber from 'bignumber.js'
 
 import { prefixArray, encodeHash } from '../utils'
+import { BytesInMb, bytesToMegabytes } from '../../src/utils'
 
 chai.use(dirtyChai)
 const expect = chai.expect
@@ -38,5 +40,24 @@ describe('Utils', function () {
     it('should reject when element is too long', () => {
       expect(() => prefixArray(['qwertyuiopqwertyuiopwertyuiopqwertyuiop'], 'prefix')).to.throw('Element 0 was longer then expected!')
     })
+  })
+  describe('bytesToMb', () => {
+    const cases: Array<[number | string | BigNumber, BigNumber]> = [
+      [BytesInMb, new BigNumber(1)],
+      [BytesInMb * 3, new BigNumber(3)],
+      [`${BytesInMb * 2}`, new BigNumber(2)],
+      [new BigNumber(BytesInMb * 0.5), new BigNumber(0.5)],
+      [new BigNumber(BytesInMb * 100), new BigNumber(100)],
+      [BytesInMb * 2.3, new BigNumber(2.3)],
+      [BytesInMb * 0.1, new BigNumber(0.1)],
+      [new BigNumber(BytesInMb * 250), new BigNumber(250)]
+    ]
+    cases.forEach(
+      ([argument, expected]) => {
+        it(`${argument.toString()} bytes should be ${expected.toString()} MB`, () => {
+          expect(bytesToMegabytes(argument)).to.be.eql(expected)
+        })
+      }
+    )
   })
 })
