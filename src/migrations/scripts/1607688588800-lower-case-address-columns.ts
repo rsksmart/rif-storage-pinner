2c923ebe82514079b4b614ba2d5f96258b198e3d
Nazar Duchak <nazar@iovlabs.org>
2020-12-11 14:12:05 +0200

    feat: add migration for lowecasing

---
+++ src/migrations/scripts/1607688588800-lower-case-address-columns.ts
@@ -0,0 +1,22 @@
+import { QueryInterface } from 'sequelize'
+import { Sequelize as SequelizeTs } from 'sequelize-typescript'
+
+export default {
+  // eslint-disable-next-line require-await
+  async up (queryInterface: QueryInterface, sequelize: SequelizeTs): Promise<void> {
+    const transaction = await sequelize.transaction()
+    try {
+      await sequelize.query('UPDATE data-store SET offerId = lower(offerId)', { transaction })
+      await sequelize.query('UPDATE storage_agreement SET consumer = lower(consumer), tokenAddress = lower(tokenAddress)', { transaction })
+
+      await transaction.commit()
+    } catch (e) {
+      await transaction.rollback()
+      throw e
+    }
+  },
+  // eslint-disable-next-line require-await
+  async down (queryInterface: QueryInterface, sequelize: SequelizeTs): Promise<void> {
+    return Promise.resolve()
+  }
+}
